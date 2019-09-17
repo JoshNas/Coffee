59978231b71a6de7a82fcb4715e4e6f62cbb2f13
JoshNas <36458322+JoshNas@users.noreply.github.com>
2019-09-17 11:31:06 -0400

    order page

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
 
 
-class LoginScreen(GridLayout):
-
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-
-        # Create and add name input
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-
-        # Create and add password input
-        self.add_widget(Label(text='Password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
-
-        self.login_button = Button(text='Log In')
-        self.login_button.bind(on_press=self.login)
-        self.add_widget(self.login_button)
-
-    def login(self, instance):
-        print(f'{self.username.text} logging in with {self.password.text}')
+class SimplePopup(Popup):
+    pass
 
 
-class MainApp(App):
+class LoginScreen(Screen):
 
+    def login(self):
+        if self.ids.username.text == '' and self.ids.password.text == '':
+            # manager is the screen manager, current sets name of screen to display
+            self.manager.current = 'order'
+        else:
+            self.ids.username.text = ''
+            self.ids.password.text = ''
+            SimplePopup().open()
+
+
+class OrderScreen(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+kv = Builder.load_file("main.kv")
+
+
+class CoffeeApp(App):
     def build(self):
-        self.title = 'Welcome to Coffee Shop'
-        return LoginScreen()
+        return kv
 
 
 if __name__ == "__main__":
-    app = MainApp()
-    app.run()
+    CoffeeApp().run()
